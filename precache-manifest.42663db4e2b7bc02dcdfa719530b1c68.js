9b2c06926f40959e7d3f6353a6a4a29b9d172318
Rishabh <rmalhotra_be18@thapar.edu>
2020-07-18 23:46:23 +0530

    Updates

---
+++ precache-manifest.42663db4e2b7bc02dcdfa719530b1c68.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612e0d9b476ec7d65a06e8fe5d65e7d3",
+    "url": "/miniature-octo-spork/index.html"
+  },
+  {
+    "revision": "78427e68dd1a81090c97",
+    "url": "/miniature-octo-spork/static/css/main.48bf5410.chunk.css"
+  },
+  {
+    "revision": "ad2f6ceecde8d5ce60de",
+    "url": "/miniature-octo-spork/static/js/2.7eac866d.chunk.js"
+  },
+  {
+    "revision": "9110c923800987d12d856da8548c4502",
+    "url": "/miniature-octo-spork/static/js/2.7eac866d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78427e68dd1a81090c97",
+    "url": "/miniature-octo-spork/static/js/main.2c6cc3e0.chunk.js"
+  },
+  {
+    "revision": "0020b7b6a049b793ec7a",
+    "url": "/miniature-octo-spork/static/js/runtime-main.9eaf8f0e.js"
+  },
+  {
+    "revision": "f331566e716a10078fb641ed22416987",
+    "url": "/miniature-octo-spork/static/media/1.f331566e.png"
+  },
+  {
+    "revision": "72004132d7eedc9690900d5f30a6c89b",
+    "url": "/miniature-octo-spork/static/media/2.72004132.png"
+  },
+  {
+    "revision": "778c2c3fec31a604f731466fcb45ca0f",
+    "url": "/miniature-octo-spork/static/media/3.778c2c3f.png"
+  },
+  {
+    "revision": "f8c662e07d8fb98aa551586420bc91de",
+    "url": "/miniature-octo-spork/static/media/4.f8c662e0.png"
+  },
+  {
+    "revision": "56a2aaea761da4954fab8343899bbb6b",
+    "url": "/miniature-octo-spork/static/media/5.56a2aaea.png"
+  },
+  {
+    "revision": "0a870f6bff8c1cb8903b28b7927f2943",
+    "url": "/miniature-octo-spork/static/media/6.0a870f6b.png"
+  },
+  {
+    "revision": "f286450f4924d4e696c20e19a0bbc7c3",
+    "url": "/miniature-octo-spork/static/media/7.f286450f.png"
+  },
+  {
+    "revision": "1ba33cc84d9e4ee60ab7bd3ee12c4474",
+    "url": "/miniature-octo-spork/static/media/dp-2.1ba33cc8.jpg"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/miniature-octo-spork/static/media/menu.4aaf92d0.svg"
+  }
+]);
